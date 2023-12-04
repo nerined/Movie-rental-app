6b68442f0396930b39ef6d85a54cd3828679abbd
Nedzinskiene <neringa.nedzinskiene@accenture.com>
2023-12-04 19:24:16 +0200

    init: add js file

---
+++ login/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const emailSigin = document.querySelector("#email");
+const passwordSigin = document.querySelector("#current-password");
+const userName = document.querySelector("#new-name");
+const userSurname = document.querySelector("#new-surname");
+const emailSignup = document.querySelector("#new-email");
+const emailRepeat = document.querySelector("#repeat-email");
+const passwordSignup = document.querySelector("#new-password");
+const passwordRepeat = document.querySelector("#repeat-password");
